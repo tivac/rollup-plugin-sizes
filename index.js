04942e4334e2c151b94df463ec6e3d5e9e4ef021
thdk <t.dekiere@gmail.com>
2019-02-23 11:55:52 +0100

    Fix compatiblity with rollup 1.2.2
    bundle.modules are no longer an array. Instead it became on object
    dictionary.
    
    Fixes tivac/rollup-plugin-sizes#10

--- index.js
+++ index.js
@@ -2,34 +2,35 @@
 
 var path = require("path"),
 
-    each     = require("lodash.foreach"),
-    sum      = require("lodash.sumby"),
-    parse    = require("module-details-from-path"),
+    each = require("lodash.foreach"),
+    sum = require("lodash.sumby"),
+    parse = require("module-details-from-path"),
     filesize = require("filesize");
 
 function defaultReport(details) {
     var args = Object.assign({}, details);
-    
+
     // Sort
     args.totals.sort((a, b) => b.size - a.size);
     console.log("%s:", args.input);
 
     args.totals.forEach((item) => {
+        const itemSize = item.size || 0;
         console.log(
             "%s - %s (%s%%)",
             item.name,
-            filesize(item.size),
-            ((item.size / args.total) * 100).toFixed(2)
+            filesize(itemSize),
+            ((itemSize / args.total) * 100).toFixed(2)
         );
 
-        if(args.options.details) {
+        if (args.options.details) {
             args.data[item.name]
                 .sort((a, b) => b.size - a.size)
                 .forEach((file) => console.log(
                     "\t%s - %s (%s%%)",
                     file.path,
-                    filesize(file.size),
-                    ((file.size / item.size) * 100).toFixed(2)
+                    filesize(file.size || 0),
+                    ((file.size / itemSize) * 100).toFixed(2)
                 ));
         }
     });
@@ -38,54 +39,56 @@ function defaultReport(details) {
 module.exports = (options) => {
     var input, base, report;
 
-    if(!options) {
+    if (!options) {
         options = false;
     }
 
     report = (options && options.report) || defaultReport;
 
     return {
-        name : "rollup-plugin-sizes",
+        name: "rollup-plugin-sizes",
 
         // Grab some needed bits out of the options
-        options : (config) => {
+        options: (config) => {
             input = config.input;
-            base  = path.dirname(config.input);
+            base = path.dirname(config.input);
         },
 
         // Spit out stats during bundle generation
-        ongenerate : (details) => {
-            var data   = {},
+        ongenerate: (details) => {
+            var data = {},
                 totals = [],
-                total = 0;
+                total = 0,
+                ids = Object.keys(details.bundle.modules);
 
-            details.bundle.modules.forEach((module) => {
+            ids.forEach((id) => {
+                const module = details.bundle.modules[id];
                 var parsed;
 
                 // Handle rollup-injected helpers
-                if(module.id.indexOf("\u0000") === 0) {
+                if (id.indexOf("\u0000") === 0) {
                     parsed = {
-                        name    : "rollup helpers",
-                        basedir : "",
-                        path    : module.id.replace("\u0000", "")
+                        name: "rollup helpers",
+                        basedir: "",
+                        path: id.replace("\u0000", "")
                     };
                 } else {
-                    parsed = parse(module.id);
+                    parsed = parse(id);
 
-                    if(!parsed) {
+                    if (!parsed) {
                         parsed = {
-                            name    : "app",
-                            basedir : base,
-                            path    : path.relative(base, module.id)
+                            name: "app",
+                            basedir: base,
+                            path: path.relative(base, id)
                         };
                     }
                 }
 
-                if(!(parsed.name in data)) {
+                if (!(parsed.name in data)) {
                     data[parsed.name] = [];
                 }
 
-                data[parsed.name].push(Object.assign(parsed, { size : module.code.length }));
+                data[parsed.name].push(Object.assign(parsed, { size: module.originalLength }));
             });
 
             // Sum all files in each chunk
